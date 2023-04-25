6c37c80b053da60205aa595b9a607008b5aea1cf
Anna-Khobta <ana14i88@gmail.com>
2023-04-25 22:23:08 +0300

    func last 3 users have liked

--- src/repositories/posts-query-repositories.ts
+++ src/repositories/posts-query-repositories.ts
@@ -2,21 +2,85 @@ import {PostModelClass, UserModelClass} from "./db/db";
 import {LikeStatusesEnum, NewestLikesType, PostsWithPagination, PostViewType} from "./db/types";
 import {SortOrder} from "mongoose";
 
+export const last3UsersLikes = async (postId: string) => {
 
+    const postWithLikes = await PostModelClass.find(
+        {_id: postId, "usersEngagement.userStatus": LikeStatusesEnum.Like},
+        {_id: 0, __v: 0}
+    )
+        .sort({"usersEngagement.createdAt": "desc"})
+        .lean();
+
+    console.log(postWithLikes, "postWithLikes __ UPPER  ")
+
+
+    let mappedLikes: NewestLikesType[] = []
+
+    if (postWithLikes.length > 0) {
+        if (postWithLikes[0].usersEngagement.length > 0) {
+
+            const filteredLikes = postWithLikes[0].usersEngagement.filter(user => user.userStatus === 'Like')
+            const last3Likes = filteredLikes.slice(-3)
+            const reverse = last3Likes.reverse()
+
+            //console.log(reverse, "userLikeForMap  __ with user  ")
+
+            mappedLikes = await Promise.all(reverse.map(async element => {
+
+                const foundLogins = await UserModelClass.find({_id: element.userId}, {"accountData.login": 1})
+
+                return {
+                    addedAt: element.createdAt,
+                    userId: element.userId,
+                    login: foundLogins[0]?.accountData?.login
+                }
+
+            }))
+        }
+        return mappedLikes
+
+    } else { return mappedLikes }
+}
 
 export const postsQueryRepositories = {
 
     async findPosts(page: number, limit: number, sortDirection: SortOrder, sortBy: string, skip: number): Promise<PostsWithPagination> {
 
-        let findPosts: PostViewType[] = await PostModelClass.find(
+        // TODO Promise<any> !
+
+        let foundPosts = await PostModelClass.find(
             {},
-            {_id: 0, __v: 0})
+            {__v: 0})
             .skip(skip)
             .limit(limit)
-            .sort({sortBy: sortDirection})
+            .sort({[sortBy]: sortDirection})
             .lean()
 
-        //console.log(findPosts)
+        console.log(foundPosts, "foundPosts")
+
+        const mappedPosts= await Promise.all(foundPosts.map(async post => {
+
+            const likers = await last3UsersLikes(post._id.toString())
+
+            return {
+                id: post._id.toString(),
+                title: post.title,
+                shortDescription: post.shortDescription,
+                content: post.content,
+                blogId: post.blogId,
+                blogName: post.blogName,
+                createdAt: post.createdAt,
+                extendedLikesInfo: {
+                    likesCount: post.likesCount,
+                    dislikesCount: post.dislikesCount,
+                    myStatus: LikeStatusesEnum.None,
+                    newestLikes: likers
+                }
+            }
+        }))
+
+        console.log(mappedPosts, "mappedPosts")
+
 
         const total = await PostModelClass.countDocuments()
         const pagesCount = Math.ceil(total / limit)
@@ -26,7 +90,7 @@ export const postsQueryRepositories = {
             page: page,
             pageSize: limit,
             totalCount: total,
-            items: findPosts
+            items: mappedPosts
         }
 
     },
@@ -40,6 +104,8 @@ export const postsQueryRepositories = {
                 return null
             }
 
+            const likers = await last3UsersLikes(post._id.toString())
+
             const postView = {
                 id: post._id.toString(),
                 title: post.title,
@@ -52,7 +118,7 @@ export const postsQueryRepositories = {
                     likesCount: post.likesCount,
                     dislikesCount: post.dislikesCount,
                     myStatus: LikeStatusesEnum.None,
-                    newestLikes: post.usersEngagement
+                    newestLikes: likers
                 }
             }
 
@@ -90,7 +156,7 @@ export const postsQueryRepositories = {
             .sort({"usersEngagement.createdAt": "desc"})
             .lean();
 
-        console.log(postWithLikes, "postWithLikes __ with user ")
+        //console.log(postWithLikes, "postWithLikes __ with user ")
 
 
         let mappedLikes: NewestLikesType[] = []
@@ -102,7 +168,7 @@ export const postsQueryRepositories = {
                 const last3Likes = filteredLikes.slice(-3)
                 const reverse = last3Likes.reverse()
 
-                console.log(reverse, "userLikeForMap  __ with user  ")
+                //console.log(reverse, "userLikeForMap  __ with user  ")
 
                 mappedLikes = await Promise.all(reverse.map(async element => {
 
@@ -115,8 +181,6 @@ export const postsQueryRepositories = {
                     }
 
                 }))
-
-                //console.log(mappedLikes, "mappedLikes")
             }
         }
 
@@ -124,10 +188,6 @@ export const postsQueryRepositories = {
 
         // если у нас нет userID
         //TODO mmm
-
-
-
-
 
         const postView = {
             id: postId,
@@ -166,7 +226,7 @@ export const postsQueryRepositories = {
             .limit(3)
             .lean();
 
-        console.log(postWithLikes, "postWithLikes1 _ findPostWithoutUser")
+       // console.log(postWithLikes, "postWithLikes1 _ findPostWithoutUser")
 
         let mappedLikes: NewestLikesType[] = []
 
@@ -177,7 +237,7 @@ export const postsQueryRepositories = {
                 const last3Likes = filteredLikes.slice(-3)
                 const reverse = last3Likes.reverse()
 
-                    console.log(reverse, "last3Likes __without user ")
+                   // console.log(reverse, "last3Likes __without user ")
 
                 mappedLikes = await Promise.all(reverse.map(async element => {
 
@@ -192,7 +252,7 @@ export const postsQueryRepositories = {
             }
         }
 
-        console.log(mappedLikes, "mappedLikes _ without user id")
+      //  console.log(mappedLikes, "mappedLikes _ without user id")
 
 
         const postView = {
